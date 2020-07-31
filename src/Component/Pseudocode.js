9c0643b7d9fa853e9a00cde03ee1bb5edc45cd45
sindhubedare <sindhubedare1990@gmail.com>
2020-07-30 21:08:10 -0500

    pseudocode

---
+++ src/Component/Pseudocode.js
@@ -0,0 +1,6 @@
+//install react-router-dom on terminal
+//import browserroute along with switch, route, link etc
+//contructor with properties in this.state - username, password, age, gender and occupation
+//create event handler and handle submit
+//render the output
+//make sure to give option of male, female and other for gender as a drop down
